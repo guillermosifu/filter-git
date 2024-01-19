ce6ac4b6e90258f36489adede6235bb2591c1218
guillermosifu <sifuentes1201@gmail.com>
2024-01-18 21:17:58 -0500

    canot

--- src/components/CustomCard/index.jsx
+++ src/components/CustomCard/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import{Avatar,Box,Card,CardContent,Grid,Typography}from "@mui/material"
 
-const CustomCard = () => {
+const CustomCard = ({user,handleClick}) => {
   return (
-    <div>CustomCard</div>
-  )
-}
+   <Box>
+    <Card>
+        <CardContent>
+            <Grid container spacing={3}>
+            <Grid item xs={1}>
+            <Avatar src={user.avatar_url}/>
+            </Grid>
+            <Grid item xs={11}>
+             <Typography sx={{cursor:'pointer'}} onClick={()=>handleClick(user.login)}>
+               {user.login}
+             </Typography>
+            </Grid>
+            </Grid>
+        </CardContent>
+    </Card>
+   </Box>
+  );
+};
 
 export default CustomCard
--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,49 +1,45 @@
-import { useState,useEffect } from "react"
-import { useParams,useNavigate } from "react-router-dom"
-import { searchUsers } from "../../services"
-import{Container,Box,Typography} from"@mui/material"
-
-
-
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { searchUsers } from "../../services";
+import { Container, Box, Typography } from "@mui/material";
+import CustomCard from "../../components/CustomCard";
 
 const Users = () => {
- const{username} =useParams();
+  const { username } = useParams();
 
- const history =useNavigate();
+  const history = useNavigate();
 
- const[userList,setUserList]=useState([])
- 
- const fetchUsers=async()=>{
-  const data = await searchUsers(username)
-  setUserList(data.items)
-  console.log(data)
- }
+  const [userList, setUserList] = useState([]);
 
- const handleClick =(username)=>{
- history(`/user/${username}`)
- }
+  const fetchUsers = async () => {
+    const data = await searchUsers(username);
+    setUserList(data.items);
+    console.log(data);
+  };
 
- useEffect(()=>{
-fetchUsers()
- },[])
+  const handleClick = (username) => {
+    history(`/user/${username}`);
+  };
 
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   return (
     <Container>
       <Box>
         <Typography variant="h6">
-           Resultado de busqueda del usuario :@{username}
+          Resultado de busqueda del usuario :@{username}
         </Typography>
       </Box>
       <Box>
-        {userList.length > 0 && 
-        userList.map((user,index)=>(
-          algo
-        ))}
+        {userList.lenght > 0 &&
+          userList.map((user, index) => (
+            <CustomCard key={index} handleClick={handleClick} user={user} />
+          ))}
       </Box>
-
     </Container>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
